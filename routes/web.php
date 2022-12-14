4c2191a0d9b43647122e055a21a109d5c30a38f3
k-haruna <spring24.ha@icloud.com>
2022-12-14 14:10:06 +0000

    get,post

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
-    Route::get('news/create', 'add')->name('news.add')->middleware('auth');
+    Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create','create')->name('news.create');
 });
 /*[課題３]
@@ -30,9 +30,12 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
 
  //【課題４】//
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create','add')->middleware ('auth');
-    Route::get('profile/edit','edit')->middleware ('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create','add')->name('profile.add');
+    Route::post('profile/create','create')->name('profile.create');
+    
+    Route::get('profile/edit','add')->name('profile.add');
+    Route::post('profile/edit','update')->name('profile.update');
  });
  
 Auth::routes();
